704f5016bce9f3f6d98bd89c7587ba402297e24c
Yuri Golikov <meetmatt@ya.ru>
2024-10-10 18:19:05 +0200

    Added movies route

---
+++ src/Movies.tsx
@@ -0,0 +1,14 @@
+export default function Movies() {
+  return (
+    <>
+      <h1>Movies</h1>
+      <ol>
+        <li>Friend of the world</li>
+        <li>God&apos;s creatures</li>
+        <li>Confess, Fletch</li>
+        <li>Black rain</li>
+        <li>Poor things</li>
+      </ol>
+    </>
+  );
+}
